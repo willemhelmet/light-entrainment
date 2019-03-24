94466b805cffd6d284fd721a50613c26d0fe1fa9
willemhelmet <willemhelmet@gmail.com>
2019-03-24 18:14:27 -0500

    Shuffle variables and made functions

--- js/aframe-synth.js
+++ js/aframe-synth.js
@@ -1,27 +1,21 @@
+// Declare Variables
+var camera;                                         // Camera object
+var cameraRotX, cameraRotY, cameraRotZ;             // Camera rotation
+var cameraRotXNorm, cameraRotYNorm, cameraRotZNorm; // Normalized camera rotation
+var cameraPosx, cameaPosY, cameraPosZ;              // Camera position
+var cameraPosXNorm, cameraPosYNorm, cameraPosZNorm; // Normalized camera position
+
+var frequency;                                      // Base frequency for synth
+var freqDelta;                                      // Delta between two oscillators
+
+var panL, oscLFreq, oscL;                           // Left speaker oscillator
+var panR, socrFreq, oscR;                           // Right speaker oscillator
+
 AFRAME.registerComponent('synth', {
     init: function ()
-    {
-        // Get Camera Object
-        var camera = document.querySelector('#camera');
-        // Setup Camera variables
-        // cameraRotZ not used because the web-browser doesn't support Z-rotation and I'm too lazy
-        // to view in a headset
-        var cameraRotX, cameraRotY //, cameraRotZ;
-        var cameraRotXNorm;
-        // TODO: add room-tracking functionality. For now assume 3DOF.
-        //var cameraPosx, cameaPosY, cameraPosZ;
-
-        // Setup Frequency
-        var frequency = 440;
-        var delta = 10;
-
-        // Setup Oscillators
-        var panL = new Tone.Panner(-1).toMaster();
-        var panR = new Tone.Panner(1).toMaster();
-        var oscLFreq = frequency;
-        var oscRFreq = oscLFreq + delta;
-        var oscL = new Tone.Oscillator(oscLFreq, "sine").connect(panL).start();
-        var oscR = new Tone.Oscillator(oscRFreq, "sine").chain(panR).start();
+    {        
+        setupCamera();
+        setupSynth();
     },
     tick: function ()
     {
@@ -29,19 +23,41 @@ AFRAME.registerComponent('synth', {
         cameraRotX = camera.getAttribute('rotation').x;
         cameraRotXNorm = cameraRotX / 90;
         cameraRotY = camera.getAttribute('rotation').y % 360;
-        //cameraRotZ = camera.getAttribute('rotation').z;
+        cameraRotZ = camera.getAttribute('rotation').z % 360;
 
         // CURRENT SETTINGS - Try 1
         // X: Global pitch
         //      Lerp between -90 and 90
         // Y: Frequency Delta
         //      Modulo 360 then Lerp between 0 and 360
-        // Z: NOTHING - Can't do Y rotation on web
+        // Z: NOTHING - Can't do Z rotation on web
 
-        oscLFreq += cameraRotXNorm * 20;
+        updatePitch(cameraRotXNorm);
     }
 });
 
 function lerp(v0, v1, t) {
     return v0*(1-t)+v1*t
+}
+
+function setupCamera() {
+    // Get Camera Object
+    camera = document.querySelector('#camera');
+}
+
+function setupSynth() {
+    frequency = 440;
+    delta = 10;
+
+    panL = new Tone.Panner(-1).toMaster();
+    panR = new Tone.Panner(1).toMaster();
+    oscLFreq = frequency;
+    oscRFreq = oscLFreq + delta;
+    oscL = new Tone.Oscillator(oscLFreq, "sine").connect(panL).start();
+    oscR = new Tone.Oscillator(oscRFreq, "sine").chain(panR).start();
+}
+
+function updatePitch(rotation, scale) {
+    oscLFreq = frequency + rotation * scale; 
+    oscRFreq = oscLFreq + delta;
 }
